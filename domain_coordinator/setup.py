d699ccfe4e1ec2f1eb234651f5cbe6190a27c709
Peter Baughman <dblue135@yahoo.com>
2019-06-07 16:16:52 -0700

    Add ament_add_pytest_isolated and ament_add_gtest_isolated (#3)
    * ament_cmake_ros add cmake test functions with isolation
    
      - Add a domain_coordinator package containing the functionality from launch_testing and
        a run_test_isolated.py script that can be used by the isolated cmake test functions instead
        of the default run_test.py
      - Add ament_cmake_gtest_isolated to run gtests with ROS_DOMAIN_ID isolation
      - Add ament_cmake_pytest_isolated to run pytests with ROS_DOMAIN_ID isolation
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    
    * ament_cmake_ros Add doc strings for cmake test functions
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    
    * ament_cmake_test_isolated shuffle run_test_isolated into its own package
    
      - Make domain_coordinator an ament_python package
      - remove run_test_isolated.py from domain_coordinator
      - Add a new cmake package that provides run_test_isolated.py
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    
    * Address PR comments
    
      - Fix copyright date from 2017->2019
      - Simplify dependencies of isolated test packages
      - Put more information in domain_coordinator setup.py
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    
    * Add missing fields in setup.py
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    
    * Rename ament_cmake_*_isolated to ament_cmake_ros_isolated_*
    
      - Give a better name so this doesn't seem like the same thing as build isolated vs build merge install
      - Add the explanation of how ROS_DOMAIN_ID is selected in the cmake function documentation
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>

---
+++ domain_coordinator/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+package_name = 'domain_coordinator'
+
+setup(
+    name=package_name,
+    version='0.7.0',
+    packages=[
+        'domain_coordinator',
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Pete Baughman',
+    author_email='pete.baughman@apex.ai',
+    maintainer='Pete Baughman',
+    maintainer_email='pete.baughman@apex.ai',
+    url='https://github.com/ros2/ament_cmake_ros',
+    download_url='https://github.com/ros2/ament_cmake_ros/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='A tool to coordinate unique ROS_DOMAIN_IDs across multiple processes',
+    long_description=(
+        'This package provides the functionality used by ament_cmake_pytest_isolated, '
+        'ament_cmake_gtest_isolated, and launch_testing to select unique ROS_DOMAIN_IDs '
+        'that allow tests to run in parallel without interfering with each other.'
+    ),
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+)
