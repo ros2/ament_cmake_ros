9b05c5205589b364002b92f646cf85eed7cf60e0
Pete Baughman <pete.baughman@apex.ai>
2019-06-13 16:13:53 -0700

    Fix lint errors
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>

--- ament_cmake_ros/cmake/ament_add_ros_isolated_gmock.cmake
+++ ament_cmake_ros/cmake/ament_add_ros_isolated_gmock.cmake
@@ -14,12 +14,13 @@
 
 #
 # Add a gmock test that runs with ROS_DOMAIN_ID isolation.
-# If no ROS_DOMAIN_ID is set, automatically select one not in use by another isolated test
-# before running the target test.
-# This will prevent tests running in parallel from interfering with one-another.
+# If no ROS_DOMAIN_ID is set, automatically select one not in use by another
+# isolated test before running the target test.
+# This will prevent tests running in parallel from interfering with
+# one-another.
 #
 # This behavior can be disabled for debugging in two ways:
-# 1) Creating an environment variable called DISABLE_ROS_ISOLATION 
+# 1) Creating an environment variable called DISABLE_ROS_ISOLATION
 # 2) Setting a ROS_DOMAIN_ID environment variable.
 #    This will cause the tests to use that ROS_DOMAIN_ID.
 #
