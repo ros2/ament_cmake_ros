ae2686c54707c78d2dbe422ab0f179cb72f5268b
Shane Loretz <sloretz@osrfoundation.org>
2022-05-05 13:26:25 -0700

    Qualify it's only when using Fast-DDS
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ament_cmake_ros/env_hook/warn_if_not_administrator.bat
+++ ament_cmake_ros/env_hook/warn_if_not_administrator.bat
@@ -1,7 +1,7 @@
 net session >nul 2>&1
 if NOT %errorLevel% == 0 (
     echo =======
-    echo Warning: ROS 2 executables require administrator permissions on Windows.
+    echo Warning: ROS 2 executables require administrator permissions on Windows when using the default middleware (rmw_fastrtps_cpp).
     echo Programs may appear to work correctly, but might not be able communicate with other nodes.
     echo =======
 )
