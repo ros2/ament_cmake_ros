2d33dd42d4b8cb8c7a8167f05c350af50eeae6aa
Scott K Logan <logans@cottsay.net>
2025-04-15 02:17:54 -0500

    Don't set ROS_AUTOMATIC_DISCOVERY_RANGE in rmw_test_fixture (#33)
    There seems to be a bug somewhere that makes this setting misbehave
    sporadically on Windows builds. Until we can get to the bottom of that,
    we'll need to drop it.
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- rmw_test_fixture_implementation/src/rmw_test_fixture_default/rmw_test_fixture_default.c
+++ rmw_test_fixture_implementation/src/rmw_test_fixture_default/rmw_test_fixture_default.c
@@ -171,13 +171,6 @@ rmw_test_isolation_start_default()
   // Avoid ROS_DOMAIN_ID=0 entirely
   slot += 1;
 
-  if (!rcutils_set_env("ROS_AUTOMATIC_DISCOVERY_RANGE", "LOCALHOST")) {
-    fprintf(stderr, "Failed to update ROS_AUTOMATIC_DISCOVERY_RANGE\n");
-    port_lock_fini(g_lock);
-    g_lock = INVALID_PORT_LOCK;
-    return RMW_RET_ERROR;
-  }
-
   rcutils_allocator_t allocator = rcutils_get_default_allocator();
   char *env_val = rcutils_format_string(allocator, "%d", slot);
   if (NULL == env_val) {
@@ -190,9 +183,6 @@ rmw_test_isolation_start_default()
   if (!rcutils_set_env("ROS_DOMAIN_ID", env_val)) {
     fprintf(stderr, "Failed to update ROS_DOMAIN_ID\n");
     allocator.deallocate(env_val, &allocator.state);
-    if (!rcutils_set_env("ROS_AUTOMATIC_DISCOVERY_RANGE", NULL)) {
-      fprintf(stderr, "Failed to clear ROS_AUTOMATIC_DISCOVERY_RANGE\n");
-    }
     port_lock_fini(g_lock);
     g_lock = INVALID_PORT_LOCK;
     return RMW_RET_ERROR;
@@ -208,10 +198,6 @@ rmw_test_isolation_stop_default()
 {
   if (!rcutils_set_env("ROS_DOMAIN_ID", NULL)) {
     fprintf(stderr, "Failed to clear ROS_DOMAIN_ID\n");
-  }
-
-  if (!rcutils_set_env("ROS_AUTOMATIC_DISCOVERY_RANGE", NULL)) {
-    fprintf(stderr, "Failed to clear ROS_AUTOMATIC_DISCOVERY_RANGE\n");
   }
 
   port_lock_fini(g_lock);
